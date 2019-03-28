e41922189c9bf7b48da939581faec520ba72047e
codeforcer <nathancworsley@gmail.com>
2019-03-28 18:54:22 +0200

    return list of all admins in the group

--- localcoinbot.py
+++ localcoinbot.py
@@ -8,7 +8,15 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 def start(update, context):
-    context.message.reply_text('Hi!')
+    print(update)
+    print(context)
+    context.message.reply_text('Hi! My name is LocalCoinBot. How can I be of assistance?')
+
+def admins(update, context):
+    admins = update.getChatAdministrators(chat_id=context.effective_chat.id)
+    admin_names = [ '@'+i.user.username for i in admins ]
+    reply = 'The following users are the ONLY admins in this group. Do not trust anyone else:\n'+'\n'.join(admin_names)
+    context.message.reply_text(reply)
 
 def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
@@ -19,6 +27,9 @@ def main():
     dispatcher = updater.dispatcher
     # Handle /start command
     dispatcher.add_handler(CommandHandler('start', start))
+    # Handle /admin command
+    dispatcher.add_handler(CommandHandler('admins', admins))
+    dispatcher.add_handler(CommandHandler('admin', admins))
     # Error handler
     dispatcher.add_error_handler(error)
     # Start the bot and run until a kill signal arrives
